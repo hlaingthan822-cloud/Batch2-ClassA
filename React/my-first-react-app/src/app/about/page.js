5998d46fffcc481301aecc6652bffc5bedb08131
hlaingthan822-cloud <hlaingthan822@gmail.com>
2026-05-06 15:53:13 +0630

    React
    React

---
+++ React/my-first-react-app/src/app/about/page.js
@@ -0,0 +1,7 @@
+import Button from "../../components/Button";
+export default function About(){
+    return (<div>
+        <h1>About Page</h1>
+    </div>
+    );
+}
---
+++ React/my-first-react-app/src/app/page.js
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+import Button from  "../components/Button";
+export default function Home() {
+ return(
+  <div className={styles.page}>
+  <h1> Welcome from Home Page</h1>
+  <Button/>
+  </div>
+ );
+}
